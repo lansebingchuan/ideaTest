7a0d1817b299c05a6546bac721aae90d22787413
lansebingchuan <1669638693@qq.com>
2019-08-07 14:37:53 +0800

    2019/8/7

---
+++ zhongchou/src/main/java/com/zht/zhongchou/ZhongchouApplication.java
@@ -0,0 +1,13 @@
+package com.zht.zhongchou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhongchouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhongchouApplication.class, args);
+    }
+
+}
