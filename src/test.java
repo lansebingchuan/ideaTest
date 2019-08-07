422d972652b366f6b7685a8214e69e810419fcbb
lansebingchuan <1669638693@qq.com>
2019-08-07 14:31:40 +0800

    2019/8/7

--- src/test.java
+++ src/test.java
@@ -3,7 +3,7 @@
  * @create 2019-08-07 13:47
  */
 public class test {
-    public void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("你好");
     }
 }
